71d97ee90014ac88f892dabe7396f3369891c5dc
Divyanshu Kumar <divyanshuk10@gmail.com>
2024-03-05 21:57:20 +0530

    implemented parcelize

--- app/src/main/java/com/divyanshu/findyourinterest/model/Interest.kt
+++ app/src/main/java/com/divyanshu/findyourinterest/model/Interest.kt
@@ -1,5 +1,9 @@
 package com.divyanshu.findyourinterest.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Interest(
     val accessibility: Double,
     val activity: String,
@@ -8,4 +12,4 @@ data class Interest(
     val participants: Int,
     val price: Double,
     val type: String
-)
+) : Parcelable
